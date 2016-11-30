ad3013e72604d81e4cf76dff6ef73dd7772ead32
“neilburd” <neilburd@gmail.com>
2016-11-30 07:40:16 -0500

    Was able to get the light up sequence working

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 $(document).ready(function(){
   console.log("Hello World!");
 
@@ -7,100 +8,142 @@ $(document).ready(function(){
   const green = 2;
   const yellow = 3;
 
-let sequenceLength = 4;
-
-let number = Math.random();
-let number4 = Math.floor(number*4);
+let sequenceLength = 0;
 
 let sequence = [];
 let sequenceWords = [];
 
 let userArray = [];
+let currentClick;
+let clickCount;
 
-console.log(number);
-console.log(number4);
+function newGame() {
+  addToSequence();
 
-let createSequence = function() {
-  for (let i = 0; i <= sequenceLength; i++) {
+  animate(sequence);
+}
 
-    sequence.push(Math.floor(Math.random()*4))
-  }
+
+function animate(sequence){
+  // console.log(sequence); /// returns sequence
+  let i = 0;
+  let interval = setInterval(function(){
+    lightUp(sequence[i]);
+    // console.log(sequence[i]); /// returning to console the number with after time
+    i++;
+    if (i > sequence.length) {
+      clearInterval(interval);
+    }
+  }, 600);
+
+}
+
+function lightUp(color) {
+  let colorToLight = $('[data-color=' + color + ']').addClass('lightUp');
+
+  // console.log(colorToLight + " colorToLight"); /// returns Object
+
+  window.setTimeout(function() {
+    colorToLight.removeClass('lightUp');
+  }, 300);
+}
+
+function addToSequence(){
+
+  sequence.push(Math.floor(Math.random()*4));
+
   console.log(sequence);
-};
-createSequence();
+
+
+}
+// newGame();
+// let createSequence = function() {
+//   for (let i = 0; i <= sequenceLength; i++) {
+//     let rand = Math.floor(Math.random()*4);
+//     sequence.push(rand);
+//
+//     if ( rand === red ){
+//       // console.log("red")
+//       sequenceWords.push("red ");
+//     } else if ( rand === green ){
+//       // console.log("green");
+//       sequenceWords.push("green ");
+//     } else if ( rand === blue ){
+//       // console.log("blue");
+//       sequenceWords.push("blue ");
+//     } else if ( rand === yellow ){
+//       // console.log("yellow");
+//       sequenceWords.push("yellow ");
+//     };
+//
+//   };
+// };
+
+// createSequence();
 console.log(sequence);
+/// put the sequence on the screen
+$("h2").html(sequenceWords);
 
-
- let playSequence = function(){
-
-   for (let i = 0; i <= sequence.length; i++) {
-      if ( sequence[i] === red ){
-        console.log("red")
-        sequenceWords.push("red ");
-        ////animate the play in and out size of div of color
-      } else if ( sequence[i] === green ){
-        console.log("green");
-        sequenceWords.push("green ");
-        ////animate the play in and out size of div of color
-      } else if ( sequence[i] === blue ){
-        console.log("blue");
-        sequenceWords.push("blue ");
-        ////animate the play in and out size of div of color
-      } else if ( sequence[i] === yellow ){
-        console.log("yellow");
-        sequenceWords.push("yellow ");
-        ////animate the play in and out size of div of color
-      }
-   }
- };
-playSequence();
-console.log(sequenceWords);
-$("h2").html(sequenceWords)
+$('#startGame').on('click', newGame);
 
 let pushUserserArray = function(){
-  console.log("Click WORKS");
 
   if ($(this).hasClass('red')){
     console.log("RED");
     userArray.push(red);
+    currentClick = red;
+    checkLength();
+    // checkIfSame();
+
   } else if ($(this).hasClass('blue')) {
     console.log("BLUE");
     userArray.push(blue);
+    currentClick = blue;
+    checkLength();
+    // checkIfSame();
+
   } else if ($(this).hasClass('green')) {
     console.log("GREEN");
     userArray.push(green);
+    currentClick = green;
+    checkLength();
+    // checkIfSame();
+
   } else if ($(this).hasClass('yellow')){
     console.log("YELLOW");
     userArray.push(yellow);
+    currentClick = yellow;
+
+    checkLength();
+    // checkIfSame();
   }
-
-  console.log(userArray);
-  // userArray.push($(this));
-  // console.log(userArray);
 };
-
-
-
 
 $(".red").on('click', pushUserserArray);
 $(".blue").on('click', pushUserserArray);
 $(".green").on('click', pushUserserArray);
 $(".yellow").on('click', pushUserserArray);
 
+/// CHECK IF CURRENT CLICK IS SAME
+// checkIfSame = function(){
+//   whatToCheck = userArray.length;
+//   console.log(whatToCheck);
+//
+//   if (sequence[clickCount] === currentClick){
+//     console.log("YOU GOT IT RIGHT");
+//
+//   } else {
+//     console.log("INCORRECT");
+//   };
+// };
+//// check if click completes the sequence
 
+checkLength = function(){
+  if (sequence.length === userArray.length){
+    console.log("SAME LENGTH!");
+  } else {
+    console.log("Keep Going!");
+  }
+}
 
-
-});
-//  $( "red" ).toggleClass( "red" );
-//  $( "green" ).toggleClass( "green" );
-//  $( "blue" ).toggleClass( "blue" );
-//  $( "yellow" ).toggleClass( "yellow" );
-//  if ( sequence[i] === red ){
-//    $( "red" ).toggleClass( "redPlay" );
-//  } else if ( sequence[i] === green ){
-//    $( "green" ).toggleClass( "greenPlay" );
-//  } else if ( sequence[i] === blue ){
-//    $( "blue" ).toggleClass( "bluePlay" );
-//  } else if ( sequence[i] === yellow ){
-//    $( "yellow" ).toggleClass( "yellowPlay" );
-//  }
+});////****END OF DOCUMENT READY

5f53b27d30ecb6952c627a68ba8c37e123b3678d
“neilburd” <neilburd@gmail.com>
2016-12-02 10:49:21 -0500

    working

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ console.log("Hello World!");
 let sequence = [];
 let clickCount = 0;
 let currentRound = 0;
+let speed = 600;
+let audio ;
+
 
 ///// start buttom
 $('#startGame').on('click', newGame);
@@ -17,28 +20,21 @@ $(".yellow").on('click', checkCurrentClick);
 /// ******* initialized om start button click
 function newGame() {
   resetGame();
-  /// calls new Round /// changes the text to be what the round is
   newRound();
-  ///// Animates the sequence.
-  // animate(sequence);
-console.log("--------NEW GAME---------");
-console.log("-----------------------------------");
-
+// console.log("--------NEW GAME---------");
+// console.log("-----------------------------------");
 }
 
 ///// ******** CREATES A NEW ROUND
 function newRound(){
-  // currentRound++;
+  currentRound++;
   clickCount = 1;
-  /// adds one number to the sequence
+  speed = speed - 20;
   addToSequence();
-  ///// Animates the sequence.
   animate(sequence);
-
   /// place the round on the board
   $('p').html('Round: ' + currentRound);
-
-  console.log(currentRound + " current Round @ newRound");
+  // console.log(currentRound + " current Round @ newRound");
 }
 
 /// ******** Resets the game if the user clicks an incorrect color
@@ -46,10 +42,10 @@ function resetGame() {
   sequence = [];
   currentRound = 0;
   clickCount = 1;
+  speed = 600;
   // console.log("--------  RESET THE GAME -------------");
 }
 // console.log(sequence + " sequence");
-
 
 /// ******* adds a new number to the sequence
 function addToSequence(){
@@ -76,32 +72,42 @@ function checkBoth(value1, value2) {
     // console.log(sequence.length + " sequence length @ checkBoth");
 }
 
+
+////// AUDIO
+
+function playAudio(audioID) {
+
+        let audio = $('<audio autoplay></audio>');
+          audio.append('<source src="sounds/'+audioID+'.mp3" type="audio/mp3" />');
+
+}
+
+
 /////*********  cHECKS THE USER INPUT AGAINST THE SEQUENCE AT THE CORRECT INDEX  IMPORTANT FUNCTION
 function checkCurrentClick(){
-
   let numbToCheck = sequence[clickCount-1];
   let colorClicked = $(this).data('color');
   let seqLength = sequence.length;
-
-      console.log(sequence + " ///sequence on Click");
-      console.log(sequence[clickCount] + " ///sequence @ index clickCount");
-      console.log(clickCount + " clickcount @ time it checks"); ///returns 0
-      console.log(seqLength + " sequence length variable");
-
+  let audioID = parseInt(colorClicked+1);
+// console.log(audioID + " AudioID ");
+  playAudio(audioID);
+  lightUp(colorClicked);
+      // console.log(sequence + " ///sequence on Click");
+      // console.log(sequence[clickCount] + " ///sequence @ index clickCount");
+      // console.log(clickCount + " clickcount @ time it checks"); ///returns 0
+      // console.log(seqLength + " sequence length variable");
     /// check sequence length to click count length
     if (clickCount < seqLength) { //// checks if under the sequence.length
-
       if (checkBoth(numbToCheck, colorClicked)){
           clickCount++;
-
-          console.log(clickCount + " clickCount after the values equate to the same");
-          console.log("Under Length of sequence and correct");
-          console.log("-----------------------------------");
-
+          // console.log(clickCount + " clickCount after the values equate to the same");
+          // console.log("Under Length of sequence and correct");
+          // console.log("-----------------------------------");
       } else {  ///// triggers game over
+          $('p').html('Sorry Game Over')
           resetGame();
-          console.log("Under Length of sequence and incorrect");
-          console.log("-----------------------------------");
+          // console.log("Under Length of sequence and incorrect");
+          // console.log("-----------------------------------");
       }
 
     } else if (clickCount === seqLength){ // checks if the two are the same so that the next round can be triggered
@@ -110,51 +116,41 @@ function checkCurrentClick(){
           console.log("clicks equal to Length of sequence and correct");
           console.log("-----------------------------------");
       } else {  ///// triggers game over
+          $('p').html('Sorry Game Over')
+
           resetGame();
           console.log("clicks equal to Length of sequence and incorrect");
           console.log("-----------------------------------");
       }
     }
-    // else if (clickCount > seqLength){ ///do I need this??
-    //       resetGame();
-    //       console.log("clicks over the Length of sequence");
-    //       console.log("-----------------------------------");
-    // }
-    //
   console.log(clickCount + " clickCount");
   console.log("----------- END OF CLICK ----------------");
-
 }
-
 
 ////  used some of the code found here for the animate sequence
 /////   https://codeplanet.io/building-simon-says-javascript/
-
 function animate(sequence){
   // console.log(sequence); /// returns sequence
   let i = 0;
   let interval = setInterval(function(){
+
     lightUp(sequence[i]);
-    console.log(sequence[i] + " What is being lite up"); /// returning to console the number with after time
-    console.log("------------- END OF LIGHT UP ___");
+    playAudio(sequence[i]+1);
+    // console.log(sequence[i] + " What is being lite up"); /// returning to console the number with after time
+    // console.log("------------- END OF LIGHT UP ___");
     i++;
     if (i === sequence.length) {
       clearInterval(interval);
     }
-  }, 600);
-
+  }, speed);
 }
-
 
 /// the lightUp function
 function lightUp(color) {
   let colorToLight = $('[data-color=' + color + ']').addClass('lightUp');
   window.setTimeout(function() {
     colorToLight.removeClass('lightUp');
-  }, 300);
+  }, speed/2);
 }
 ////////////////////////////////////
-
-
-
 });////****END OF DOCUMENT READY
